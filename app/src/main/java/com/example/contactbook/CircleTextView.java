9ebf702156d5d034538196a33540478c2452d0f9
Vadim Yevtushenko <vadik.chicoto@gmail.com>
2021-07-22 12:32:24 +0300

    cleared the code

--- app/src/main/java/com/example/contactbook/CircleTextView.java
+++ app/src/main/java/com/example/contactbook/CircleTextView.java
@@ -4,13 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.text.Spannable;
-import android.text.style.ImageSpan;
 import android.util.AttributeSet;
 import android.view.Gravity;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class CircleTextView extends androidx.appcompat.widget.AppCompatTextView {
     private int defaultColor = Color.GREEN;
@@ -66,50 +61,5 @@ public class CircleTextView extends androidx.appcompat.widget.AppCompatTextView 
         mPaint.setStyle(Paint.Style.FILL);
         setGravity(Gravity.CENTER);
         setColorBackground(defaultColor);
-    }
-
-    @Override
-    public void setText(CharSequence text, BufferType type) {
-        Spannable s = getTextWithImages(getContext(), text);
-        super.setText(s, BufferType.SPANNABLE);
-    }
-
-    private static final Spannable.Factory spannableFactory = Spannable.Factory.getInstance();
-
-    private static boolean addImages(Context context, Spannable spannable) {
-        Pattern refImg = Pattern.compile("\\Q[img src=\\E([a-zA-Z0-9_]+?)\\Q/]\\E");
-        boolean hasChanges = false;
-
-        Matcher matcher = refImg.matcher(spannable);
-        while (matcher.find()) {
-            boolean set = true;
-            for (ImageSpan span : spannable.getSpans(matcher.start(), matcher.end(), ImageSpan.class)) {
-                if (spannable.getSpanStart(span) >= matcher.start()
-                        && spannable.getSpanEnd(span) <= matcher.end()
-                ) {
-                    spannable.removeSpan(span);
-                } else {
-                    set = false;
-                    break;
-                }
-            }
-            String resname = spannable.subSequence(matcher.start(1), matcher.end(1)).toString().trim();
-            int id = context.getResources().getIdentifier(resname, "drawable", context.getPackageName());
-            if (set) {
-                hasChanges = true;
-                spannable.setSpan(  new ImageSpan(context, id),
-                        matcher.start(),
-                        matcher.end(),
-                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                );
-            }
-        }
-
-        return hasChanges;
-    }
-    private static Spannable getTextWithImages(Context context, CharSequence text) {
-        Spannable spannable = spannableFactory.newSpannable(text);
-        addImages(context, spannable);
-        return spannable;
     }
 }
